045606243edfbfc2d19451f70dc157ad46889639
gwilku <grzesiek.wilczek@gmail.com>
2016-04-24 19:44:29 +0200

    widok home - bootstrap

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = [
     # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
+    url(r'^$', 'kursy.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
